a100a7cea76e8966a09bc13394b4749fb7d87944
semsem93 <rla00220@gmail.com>
2020-05-26 18:30:26 +0900

    update mongodb key

---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: process.env.MONGO_URI
+    // Heroku로 배포시
+}
--- index.js
+++ index.js
@@ -4,18 +4,20 @@ const port = 5000
 const bodyParser = require('body-parser')
 const { User } = require('./models/User')
 
+const config = require("./config/key")
+
 // application/x-www-from-urlencodeted 타입으로 된 것을 분석해서 가지고 옴
 app.use(bodyParser.urlencoded({extended: true}));
 // application/json 타입으로 된 것을 분석해서 가지고 옴
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://semi:sm2200mk@cluster0-xalwm.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB connect success'))
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!!'))
+app.get('/', (req, res) => res.send('Hello World!!!'))
 
 app.post('/register', (req, res) => {
   // 회원 가입할때 필요한 정보들을 clinet 에서 가져오면

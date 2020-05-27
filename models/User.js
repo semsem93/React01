8faecaa00014be58d4f13bbd78c28ccff1928d08
semsem93 <rla00220@gmail.com>
2020-05-27 16:49:34 +0900

    install bcrpt and password 암호화:

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10; // 암호화 10자리
 
 const userSchema = mongoose.Schema({
     name: {
@@ -28,6 +30,23 @@ const userSchema = mongoose.Schema({
     },
     tokenExp: {
         type: Number
+    }
+})
+
+userSchema.pre('save', function(next){
+
+    var user = this;
+    if(user.isModified('password')){ // password가 변환 될때만
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err)
+
+            bcrypt.hash(user.password, saltRounds, function(err, hash){
+                if(err) return next(err)
+                // 암호화 성공시 password를 암호화된 password로 변경
+                user.password = hash;
+                next()
+            })
+        })
     }
 })
 
